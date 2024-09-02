10931e0db78a0e33b54cd6c0b33ad58d609b0a97
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2024-09-02 16:05:01 +0300

    Create raymarched object 4D

--- RaymarchedBoundingVolumes.Unity/Assets/Plugins/RBV.4D/Scripts/Runtime/Features/RaymarchedObject4D.cs
+++ RaymarchedBoundingVolumes.Unity/Assets/Plugins/RBV.4D/Scripts/Runtime/Features/RaymarchedObject4D.cs
@@ -1,4 +1,44 @@
-﻿namespace RBV.FourDimensional.Features
+﻿using RBV.Data.Dynamic;
+using RBV.Data.Dynamic.ShaderData;
+using RBV.Data.Dynamic.ShaderData.ObjectType;
+using RBV.Data.Static.Enumerations;
+using RBV.Features;
+using RBV.FourDimensional.Data.Dynamic.ShaderData;
+using RBV.FourDimensional.Data.Dynamic.ShaderData.ObjectType;
+using RBV.FourDimensional.Data.Static.Enumerations;
+using RBV.FourDimensional.Utilities.Wrappers;
+using RBV.Utilities.Attributes;
+using RBV.Utilities.Wrappers;
+using UnityEngine;
+
+namespace RBV.FourDimensional.Features
 {
-    public class RaymarchedObject4D { }
+    public class RaymarchedObject4D : RaymarchedObject
+    {
+        [SerializeField] private ObservableValue<RaymarchedObject4DType> type;
+        [SerializeField] private ObservableObject4DTypeShaderData        typeData;
+
+        [SerializeField, ChildTooltip(nameof(ObservableTransform<Vector4>.Scale),
+             "Warning: Non-uniform SDF scaling distorts Euclidean spaces!")]
+        private new ObservableTransform4D transform = new();
+
+        public override ObservableValue<RaymarchedObjectType> Type          { get; protected set; }
+        public override IObservableObjectTypeShaderData       TypeData      => typeData;
+        public override IObservableTransform                  Transform     => transform;
+        public override TransformType                         TransformType => TransformType.FourDimensional;
+
+        public override ITransformShaderData TransformShaderData => new Transform4DShaderData
+        {
+            Position   = (Vector4)base.transform.position + transform.Position.Value,
+            Rotation   = transform.Rotation.Value   * Mathf.Deg2Rad,
+            Rotation4D = transform.Rotation4D.Value * Mathf.Deg2Rad,
+            Scale      = transform.Scale.Value
+        };
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Type = type.Cast(enumType => (RaymarchedObjectType)(int)enumType);
+        }
+    }
 }
--- RaymarchedBoundingVolumes.Unity/Assets/Plugins/RBV.4D/Scripts/Runtime/Infrastructure/RaymarchingServicesRegister.cs
+++ RaymarchedBoundingVolumes.Unity/Assets/Plugins/RBV.4D/Scripts/Runtime/Infrastructure/RaymarchingServicesRegister.cs
@@ -1,17 +1,20 @@
-﻿using RBV.Infrastructure;
+﻿using RBV.Features.ShaderDataForming;
+using RBV.FourDimensional.Features.ShaderDataForming;
+using RBV.Infrastructure;
 
 namespace RBV.FourDimensional.Infrastructure
 {
     public class Raymarching4DServicesRegister : RaymarchingServicesRegister
     {
-        protected override void RegisterObjectTypeCaster(IServiceContainer container)
-        {
-            // container.RegisterAsSingle<IObjectTypeCaster>(new ObjectTypeCaster4D());
-        }
+        protected override void RegisterObjectTypeCaster(IServiceContainer container) =>
+            container.RegisterAsSingle<IObjectTypeCaster>(new Object4DTypeCaster());
 
-        protected override void RegisterTransformTypeCaster(IServiceContainer container)
-        {
-            // container.RegisterAsSingle<ITransformTypeCaster>(new TransformTypeCaster4D());
-        }
+        protected override void RegisterTransformTypeCaster(IServiceContainer container) =>
+            container.RegisterAsSingle<ITransformTypeCaster>(new Transform4DTypeCaster());
+
+        protected override void RegisterRaymarchedObjectShaderPropertyIdProvider(IServiceContainer container) =>
+            container.RegisterAsSingle<IRaymarchedObjectShaderPropertyIdProvider>(
+                new RaymarchedObject4DShaderPropertyIdProvider()
+            );
     }
 }
