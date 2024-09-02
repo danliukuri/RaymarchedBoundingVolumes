2de5d5ec8d5581abc91b31e98bbd5dc570bf9f67
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2024-09-02 16:03:27 +0300

    Create 4D object shader data types

---
+++ RaymarchedBoundingVolumes.Unity/Assets/Plugins/RBV.4D/Scripts/Runtime/Data/Dynamic/ShaderData/ObjectType/ObservableObject4DTypeShaderData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using RBV.Data.Dynamic;
+using RBV.Data.Dynamic.ShaderData.ObjectType;
+using RBV.FourDimensional.Data.Static.Enumerations;
+using RBV.Utilities.Wrappers;
+using UnityEngine;
+
+namespace RBV.FourDimensional.Data.Dynamic.ShaderData.ObjectType
+{
+    [Serializable]
+    public class ObservableObject4DTypeShaderData : IObservableObjectTypeShaderData
+    {
+        public event Action<ChangedValue<IObjectTypeShaderData>> Changed
+        {
+            add
+            {
+                HypersphereShaderData.Changed +=
+                    value.CastCached<IObjectTypeShaderData, RaymarchedHypersphereShaderData>();
+                HypercubeShaderData.Changed +=
+                    value.CastCached<IObjectTypeShaderData, RaymarchedHypercubeShaderData>();
+            }
+            remove
+            {
+                HypersphereShaderData.Changed -=
+                    value.CastCached<IObjectTypeShaderData, RaymarchedHypersphereShaderData>();
+                HypercubeShaderData.Changed -=
+                    value.CastCached<IObjectTypeShaderData, RaymarchedHypercubeShaderData>();
+            }
+        }
+
+        [field: SerializeField]
+        public ObservableValue<RaymarchedHypersphereShaderData> HypersphereShaderData { get; set; } =
+            new(RaymarchedHypersphereShaderData.Default);
+
+        [field: SerializeField]
+        public ObservableValue<RaymarchedHypercubeShaderData> HypercubeShaderData { get; set; } =
+            new(RaymarchedHypercubeShaderData.Default);
+
+        public IObjectTypeShaderData GetShaderData(RaymarchedObjectType type) =>
+            GetShaderData((RaymarchedObject4DType)(int)type);
+
+        public IObjectTypeShaderData GetShaderData(RaymarchedObject4DType type)
+        {
+            const float fullToHalfScaleMultiplier =
+                IObservableObjectTypeShaderData.FullToHalfScaleMultiplier;
+
+            switch (type)
+            {
+                case RaymarchedObject4DType.Hypersphere:
+                    RaymarchedHypersphereShaderData sphereShaderData = HypersphereShaderData.Value;
+                    sphereShaderData.Diameter *= fullToHalfScaleMultiplier;
+                    return sphereShaderData;
+                case RaymarchedObject4DType.Hypercube:
+                    RaymarchedHypercubeShaderData cubeShaderData = HypercubeShaderData.Value;
+                    cubeShaderData.Dimensions *= fullToHalfScaleMultiplier;
+                    return cubeShaderData;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, default);
+            }
+        }
+    }
+}
---
+++ RaymarchedBoundingVolumes.Unity/Assets/Plugins/RBV.4D/Scripts/Runtime/Data/Static/Enumerations/RaymarchedObject4DType.cs
@@ -0,0 +1,10 @@
+﻿using static RBV.FourDimensional.Data.Static.EnumerationConstants;
+
+namespace RBV.FourDimensional.Data.Static.Enumerations
+{
+    public enum RaymarchedObject4DType
+    {
+        Hypersphere = RaymarchedObject4DTypeOffset + 0,
+        Hypercube   = RaymarchedObject4DTypeOffset + 1
+    }
+}
